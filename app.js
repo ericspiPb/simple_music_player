174d50745461dc277128d7a303d8c89cd3e078a0
Eric Chan <ckc261@ha.org.hk>
2021-09-09 10:10:52 +0800

    rearrange code by relevant

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let currentMusic = 0;
-
 const music = document.querySelector('#audio');
-const seekBar = document.querySelector('.seek-bar');
+
 const songName = document.querySelector('.music-name');
 const artistName = document.querySelector('.artist-name');
-
 const disk = document.querySelector('.disk');
+
+const seekBar = document.querySelector('.seek-bar');
 const currentTime = document.querySelector('.current-time');
 const musicDuration = document.querySelector('.song-duration');
 
